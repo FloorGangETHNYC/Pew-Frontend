adefe131d6f9ddb652af768495dc3bde4982450d
jamesyzhang10 <70459407+jamesyzhang10@users.noreply.github.com>
2022-06-25 06:06:40 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Dao from "./components/Dao"
+
+
+export default function App(){
+  return(
+    <div>
+      <Navbar />
+      <Dao />
+    </div>
+  )
+}
