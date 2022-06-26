b53063e8bcf1e3ad14e808ed8fe3c0d1cb7f2e66
Kenk <kenghin_lim@hotmail.com>
2022-06-26 19:29:32 +0800

    fix

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,9 +1,9 @@
-import React from "react"
+import React from "react";
 
 export default function Footer() {
-    return(
-        <footer>
-            <p>© Pew Foundation 2022. All Flights Reserved</p> 
-        </footer>
-    )
+  return (
+    <footer>
+      <p>© Pew Foundation 2022. All Flights Reserved</p>
+    </footer>
+  );
 }
